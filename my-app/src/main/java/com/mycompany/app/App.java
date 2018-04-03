931570058c7bf1c71fee513220e3379b49288422
Harlan Brown <hbrown@nuxeo.com>
2018-04-03 16:17:34 -0500

    nightly via ssl

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -11,28 +11,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String url = "https://nightly.nuxeo.com/nuxeo";
 
-        System.out.println("A NullPointerException is a drag...");
-        Object myObject = null;
-        /**
-         * Checker prevents this from compiling...
-         */
-//        System.out.println("myObject: " + myObject.toString());
-        /**
-         * ... which is simply fantastic. It shows:
-         *
-         * error: [dereference.of.nullable] dereference of possibly-null
-         * reference myObject
-         *
-         * http://checkerframework.org
-         */
-        System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
-        String url = "http://localhost:8080/nuxeo";
         NuxeoClient nuxeoClient = new NuxeoClient.Builder().url(url).authentication("Administrator", "Administrator").connect();
-        Documents docs = nuxeoClient.operation("Repository.Query")
-                                    .param("query", "SELECT * FROM Document")
-                                    .execute();
-        System.out.println(docs.toString());
+
+        Document doc = nuxeoClient.operation("Repository.GetDocument").param("value", "/").execute();
+        System.out.println(doc);
+
+        nuxeoClient.disconnect();
     }
 }
